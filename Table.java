8eda903c337d6bce36e8deade6f489b1085d5008
Mehar Mutreja <76557895+7373Mehar@users.noreply.github.com>
2022-07-05 09:57:27 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,12 @@
+public class Table
+{
+	public static void main(String [] args)
+	{
+		int num = 5, res;
+		for(int i=1;i<=10;i++)
+		{
+		res = num*i;
+		System.out.println(num + "*" + i+ "=" +res ); 
+		}
+	} 
+}
